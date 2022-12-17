1c689d8d8ecee57e83a8500c05ee513803f2e5fe
khushnudeshtemirov <khushnudeshtemirov@gmail.com>
2022-12-17 06:20:59 +0500

    Homepage was ready

---
+++ src/pages/homepage/books.data.js
@@ -0,0 +1,74 @@
+export const book_data = [
+  {
+    id: 1,
+    imgUrl: "https://i.ibb.co/7pFCC2Z/18.png",
+  },
+  {
+    id: 2,
+    imgUrl: "https://i.ibb.co/L5MwSKK/17.png",
+  },
+  {
+    id: 3,
+    imgUrl: "https://i.ibb.co/g3FS11t/16.png",
+  },
+  {
+    id: 4,
+    imgUrl: "https://i.ibb.co/Wz8FbLg/15.png",
+  },
+  {
+    id: 5,
+    imgUrl: "https://i.ibb.co/Db0rZgd/14.png",
+  },
+  {
+    id: 6,
+    imgUrl: "https://i.ibb.co/ZfH56zQ/13.png",
+  },
+  {
+    id: 7,
+    imgUrl: "https://i.ibb.co/8DfZL8J/11.png",
+  },
+  {
+    id: 8,
+    imgUrl: "https://i.ibb.co/c6Fc3ML/12.png",
+  },
+  {
+    id: 9,
+    imgUrl: "https://i.ibb.co/WpGDnVP/10.png",
+  },
+  {
+    id: 10,
+    imgUrl: "https://i.ibb.co/7tWsy3r/9.png",
+  },
+  {
+    id: 11,
+    imgUrl: "https://i.ibb.co/LNvwZ0Z/8.png",
+  },
+  {
+    id: 12,
+    imgUrl: "https://i.ibb.co/kHxH3GN/7.png",
+  },
+  {
+    id: 13,
+    imgUrl: "https://i.ibb.co/y8g8yhQ/6.png",
+  },
+  {
+    id: 14,
+    imgUrl: "https://i.ibb.co/2d925w3/5.png",
+  },
+  {
+    id: 15,
+    imgUrl: "https://i.ibb.co/PD28DZx/4.png",
+  },
+  {
+    id: 16,
+    imgUrl: "https://i.ibb.co/j3zyzCh/3.png",
+  },
+  {
+    id: 17,
+    imgUrl: "https://i.ibb.co/W0QndKC/1.png",
+  },
+  {
+    id: 18,
+    imgUrl: "https://i.ibb.co/4ZVy67C/2.png",
+  },
+];
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Typography } from "@mui/material";
 
+import { book_data } from "./books.data";
+
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import "./homepage.scss";
 
-const Homepage = () => (
-  <div className="homepage">
-    <Link to="/all-book">All Books</Link>
-  </div>
-);
+const Homepage = () => {
+  let [move, changeMove] = useState(0);
+
+  const prevBtn = () => {
+    move == 0 ? changeMove(0) : changeMove(move + 1);
+  };
+
+  const nextBtn = () => {
+    move < -1 ? changeMove(-2) : changeMove(move - 1);
+  };
+
+  return (
+    <div className="homepage">
+      <ArrowBackIosIcon onClick={prevBtn} />
+      <ArrowForwardIosIcon onClick={nextBtn} />
+      <div className="books-container">
+        <div
+          className="books"
+          style={{ transform: `translateX(${move * 20}%)` }}
+        >
+          {
+            (console.log(move),
+            book_data.map((book) => (
+              <div className="image" key={book.id}>
+                <img src={book.imgUrl} alt="books" />
+              </div>
+            )))
+          }
+        </div>
+      </div>
+      <Link to="/all-book">
+        <Typography variant="h5">View All Books</Typography>
+      </Link>
+    </div>
+  );
+};
 
 export default Homepage;
